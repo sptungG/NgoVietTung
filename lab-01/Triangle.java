d6bcc40189610b607c19cada24cc22f46ecca369
sptungG <ngoviettung154@gmail.com>
2021-02-27 19:47:32 +0700

    ex6.3

--- lab-01/Triangle.java
+++ lab-01/Triangle.java
@@ -1,7 +1,6 @@
 /*
 6.3
 Write a program to display a triangle with a height of n stars (*), n is entered by users.
-
 */
 
 import java.util.Scanner;
@@ -9,7 +8,7 @@ import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter height: n = ");
+        System.out.print("Enter height: n = ");
         int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < n-i; j++) {
