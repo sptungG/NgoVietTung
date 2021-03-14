1397ad0680d7f387b658dec7b786feb98214025d
sptungG <ngoviettung154@gmail.com>
2021-03-14 19:56:11 +0700

    update Q&A

---
+++ lab-03/AimsProject/bin/aims/Q&A.md
@@ -0,0 +1,5 @@
+## Question:
+    If you create a constructor method to build a DVD by title then create a constructor method to build a DVD by category.
+    Does JAVA allow you to do this?
+
+`Answer:` No, since JAVA won't know that the user created a constructor method to build a DVD by category or title
--- lab-03/AimsProject/src/aims/DigitalVideoDisc.java
+++ lab-03/AimsProject/src/aims/DigitalVideoDisc.java
@@ -105,7 +105,4 @@ public class DigitalVideoDisc {
 			return false;
 		return true;
 	}
-		//Question:If you create a constructor method to build a DVD by title then create a constructor method to build a DVD by category.
-		//Does JAVA allow you to do this?
-		//Answer: No, since JAVA won't know that the user created a constructor method to build a DVD by category or title
 }
