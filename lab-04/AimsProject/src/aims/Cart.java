a34294f9f8fe80cd912141a36f56b3ece16f512b
sptungG <ngoviettung154@gmail.com>
2021-03-20 05:18:32 +0700

    update lab04

--- lab-04/AimsProject/src/aims/Cart.java
+++ lab-04/AimsProject/src/aims/Cart.java
@@ -61,7 +61,7 @@ public class Cart {
             System.out.println("----The cart is empty!");
         } else {
             System.out.println("\n----Your Cart: ");
-            System.out.println("|     |      _Title_      |      _Category_      |   _Director_   | _Length_ |      _Cost_    |");
+            System.out.println("|    |                _Title_                |      _Category_      |   _Director_   |    _Length_    |  _Cost_  |");
             for (int i = 0; i < itemsOrdered.size(); i++) {
                 String order = String.valueOf(i + 1);
                 showOrder(5, order);
---
+++ lab-04/AimsProject/src/aims/DVDUtils.java
@@ -0,0 +1,17 @@
+package aims;
+
+public class DVDUtils {
+	
+	public static void compareByCost(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		
+	}
+	public void compareByTitle(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		
+	}
+	public void sortByCost(DigitalVideoDisc[] dvdList) {
+		
+	}
+	public void sortByTitle(DigitalVideoDisc[] dvdList) {
+		
+	}
+}
