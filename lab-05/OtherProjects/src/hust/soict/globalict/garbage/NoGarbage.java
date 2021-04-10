ae2a66303d53d9d200090dc5fc15bd70c99a7472
sptungG <ngoviettung154@gmail.com>
2021-04-10 18:36:55 +0700

    update lab05

--- lab-05/OtherProjects/src/hust/soict/globalict/garbage/NoGarbage.java
+++ lab-05/OtherProjects/src/hust/soict/globalict/garbage/NoGarbage.java
@@ -1,6 +1,7 @@
 package hust.soict.globalict.garbage;
 
 public class NoGarbage {
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		long startTime, endTime;
 		startTime =System.currentTimeMillis();
